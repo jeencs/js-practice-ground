07bddff3292e094e09581945f46c07620da0ab63
Jeencs <naibzada05@gmail.com>
2025-02-21 00:08:04 +0300

    setting timer

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ for (let x = 0; x < chessCells.length; x++) {
 }
 */
 
+/**   This section is about drap and drop **/
 function allowDrop(ev) {
   ev.preventDefault();
 }
@@ -24,3 +25,36 @@ function drop(ev) {
   var data = ev.dataTransfer.getData("text");
   ev.target.appendChild(document.getElementById(data));
 }
+
+
+/** This section is about timer */
+var time = 0;
+var timerHandler;
+
+function increaseTime() {
+	time = time + 1;
+	
+	var second = time % 60;
+	var minute = Math.floor(time / 60);
+	
+	if (minute < 10) {
+		minute = "0" + minute;
+	}
+	
+	if (second < 10) {
+		second = "0" + second;
+	}
+	
+	document.getElementById("minute").innerHTML = minute;
+	document.getElementById("second").innerHTML = second;
+}
+
+function startGame() {
+	timerHandler = setInterval(increaseTime, 1000);
+}
+
+function pauseGame() {
+	clearInterval(timerHandler);
+}
+
+
